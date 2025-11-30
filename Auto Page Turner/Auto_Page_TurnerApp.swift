eaf748c4de2a7a0b07706aedeb42def40e06e9db
EtanHuang <93726471+EtanHuang@users.noreply.github.com>
2025-11-29 20:34:26 -0800

    Initial Commit

---
+++ Auto Page Turner/Auto_Page_TurnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_Page_TurnerApp.swift
+//  Auto Page Turner
+//
+//  Created by Etan Huang on 2025-11-29.
+//
+
+import SwiftUI
+
+@main
+struct Auto_Page_TurnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
